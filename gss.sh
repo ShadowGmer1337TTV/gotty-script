376baf8b2158dfa72611fe3bb2dc0057b41cca1e
ShadowGmer1337TTV <135052485+ShadowGmer1337TTV@users.noreply.github.com>
2023-05-30 19:14:21 +0200

    gss.sh

--- gss.sh
+++ gss.sh
@@ -1,2 +1,10 @@
+#gets gotty-tar archive
 wget -qO gotty.tar.gz https://github.com/yudai/gotty/releases/latest/download/gotty_linux_amd64.tar.gz
+
+#unpack gotty archive
 sudo tar xf gotty.tar.gz -C /usr/local/bin
+
+#checks if gotty installed
+#if you dont see this something went wrong
+gotty --version
+
